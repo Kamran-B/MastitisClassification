e6e10c07e6f5a556efc1ddb0db663cb2db592e7b
BeckSchemenauer <113921526+BeckSchemenauer@users.noreply.github.com>
2024-02-14 15:18:09 -0800

    added the code to do things with files

--- Test.py
+++ Test.py
@@ -1,32 +1,48 @@
-'''df = pd.read_csv('/Users/gavinlynch04/Desktop/CSC487_Project_Data/data/output.csv', nrows=5, header=0)
-
-# Extract and print the first row
-first_line = df.iloc[3]
-print(first_line)'''
-
 import dask
 dask.config.set({'dataframe.query-planning': True})
-import dask.dataframe as dd
-from dask.diagnostics import ProgressBar
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
 print("about to load data")
 # Load your dataset
-file_path = '/Users/gavinlynch04/Desktop/CSC487_Project_Data/data/merged.csv'
+file_path = 'output_hd_exclude.raw'
 
-with ProgressBar():
-    df = dd.read_csv(file_path, sample=5000000, blocksize=100e6).head(5).compute()
+# Assuming your data is comma-separated
+delimiter = ','
+
+# Read the raw file and create a 2D array
+data = []
+i = 1
+with open(file_path, 'r') as file:
+    # Skip the header if present
+    header = file.readline()
+
+    # Read the data into a 2D array
+    for line in file:
+        print("Line " + str(i) + " done.")
+        row = line.strip().split(delimiter)
+        data.append(row)
+        i += 1
 
 print("Done Loading CSV")
 
+print(data)
+
 # Assuming the last column is the target variable
-X = df.iloc[:, 6:-1].values
-y = df.iloc[:, -1].values
+X = [row[6:-1] for row in data]
+y = [row[-1] for row in data]
 
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+print(X_train)
+
+#Ensure that X_train and X_test have at least one feature
+if not X_train or not any(X_train):
+    raise ValueError("X_train does not contain valid features.")
+if not X_test or not any(X_test):
+    raise ValueError("X_test does not contain valid features.")
 
 print("about to train model")
 # Create a random forest classifier
