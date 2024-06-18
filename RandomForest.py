4df2f96edd20f9a118908b7c93d50414802b5503
Gavin Lynch <34323734+GavinLynch04@users.noreply.github.com>
2024-06-18 14:46:11 -0700

    Update RandomForest.py
    edited the printed statistics

--- RandomForest.py
+++ RandomForest.py
@@ -2,10 +2,22 @@ import random
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, classification_report
 from to_array import bit_reader
 
+def read_numbers_from_file2(file_path):
+    numbers = []
+    try:
+        with open(file_path, 'r') as file:
+            for line in file:
+                # Convert each line to a list of numbers and append to the numbers list
+                row = list(map(int, line.strip().split()))
+                numbers.append(row)
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    return np.array(numbers)
 
 def read_numbers_from_file(file_path):
     numbers = []
@@ -20,9 +32,14 @@ def read_numbers_from_file(file_path):
         print(f"An error occurred: {e}")
     return numbers
 
+herd = read_numbers_from_file2('breed_herdxyear_lact1_sorted.txt')
 
-X = bit_reader("output_hd_exclude_binary.txt")
-y = read_numbers_from_file('mast_lact1_sorted.txt')
+X = bit_reader("output_hd_exclude_4000top_SNPs_binary.txt")
+y = read_numbers_from_file('mast_lact1_sorted_herd.txt')
+
+for rowX, rowH in zip(X, herd):
+    for value in rowH:
+        rowX.append(value)
 
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -31,7 +48,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 del X, y
 
 '''Iterates through the training data and adds rows for oversampling'''
-
 
 # Function to duplicate rows and insert at random positions
 def duplicate_and_insert(original_list, target_list, original_target_labels, target_labels, label_value, num_duplicates,
@@ -59,11 +75,11 @@ y_test_augmented = y_test.copy()
 duplicate_and_insert(X_test, X_test_augmented, y_test, y_test_augmented, 1, 16, seed=seed_value)
 
 # Deletes from memory to free up RAM space
-del X_train, X_test, y_train, y_test
+del X_train, y_train
 
 # Set the parameters based on the research findings
-n_trees = 30  # Ntree
-mtry_fraction = 0.01  # Mtry as a fraction of the total number of predictors (0.01 seems to be best)
+n_trees = 1000  # Ntree
+mtry_fraction = 0.005  # Mtry as a fraction of the total number of predictors (0.005 seems to be best)
 
 # Calculate the actual Mtry value based on the fraction
 num_predictors = len(X_train_augmented[0])
@@ -77,13 +93,13 @@ random_forest_model = RandomForestClassifier(
     random_state=42,
     min_samples_split=10,
     min_samples_leaf=5,
-    max_depth=10,
+    max_depth=12,
     class_weight={0: 1, 1: 4000},
     oob_score=True,
-    verbose=2,
+    verbose=1,
 
     # If you have issues try changing this to 1
-    n_jobs=4,
+    n_jobs=-1,
 )
 
 # Train the model
@@ -94,32 +110,18 @@ y_pred = random_forest_model.predict(X_test_augmented)
 
 # Evaluate the model
 accuracy = accuracy_score(y_test_augmented, y_pred)
-print(y_test_augmented)
-print(y_pred)
 print(f'Test Accuracy: {accuracy}')
 
-feature_importances = random_forest_model.feature_importances_
+# Create and print a classification report
+report = classification_report(y_test_augmented, y_pred, target_names=["No mastitis (Control)", "Mastitis Present (Case)"])
+print(report)
+
+feature_importance = random_forest_model.feature_importances_
 
 # Threshold for the importance of SNPs
-threshold = 0.005
+threshold = 0.00000001
 
 # Identify important SNP indices based on importance scores
-important_snp_indices = [i for i, importance in enumerate(feature_importances) if importance > threshold]
-
-# Extract the corresponding SNPs from your dataset (assuming your_snp_list is a list of lists)
-important_snps = [[cow[i] for i in important_snp_indices] for cow in X_train_augmented + X_test_augmented]
-
-# Assuming you have a list of SNP labels (e.g., ['SNP1', 'SNP2', ...])
-snp_labels = ['SNP{}'.format(i + 1) for i in range(len(important_snp_indices))]
-
-# Plotting the importance scores
-plt.figure(figsize=(10, 6))
-plt.bar(snp_labels, [feature_importances[i] for i in important_snp_indices], color='blue', alpha=0.7)
-plt.title('Importance Scores of Important SNPs')
-plt.xlabel('SNP Indices')
-plt.ylabel('Importance Scores')
-plt.xticks(rotation=45, ha='right')
-plt.tight_layout()
-
-# Show or save the plot
-plt.show()
+important_snp_indices = [i for i, importance in enumerate(feature_importance) if importance > threshold]
+print("Indices of important SNPs identified by RF: " + str(important_snp_indices))
+print("Length of indices of important SNPs identified by RF: " + str(len(important_snp_indices)))
