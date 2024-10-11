77c9f437d63e0eddacb20d64d24a0d706f4d4793
BeckSchemenauer <113921526+BeckSchemenauer@users.noreply.github.com>
2024-10-10 21:42:47 -0700

    big time refactoring and cleaning

--- DataQuality/compressor.py
+++ DataQuality/compressor.py
@@ -1,9 +1,9 @@
 import os
 from tqdm import tqdm
-import concurrent.futures
+from DataQuality.funtional_consequences import load_1d_array_from_file
 
 
-# Reads the first column from three input files, finds the intersection of values in those columns, and returns the lists and their intersection.
+# Reads the first column from two input files, finds the intersection of values in those columns, and returns the lists and their intersection.
 def read_and_intersect(file1_path, file2_path):
     def read_first_column(file_path):
         with open(file_path, "r") as file:
@@ -25,7 +25,7 @@ def filter_and_copy(file_path, intersection_list, output_path):
     with open(file_path, "r") as input_file, open(output_path, "w") as output_file:
         lines = input_file.readlines()
         for line in tqdm(
-            lines, desc=f"Filtering {file_path} and copying to {output_path}"
+                lines, desc=f"Filtering {file_path} and copying to {output_path}"
         ):
             first_value = line.split()[0]
             if first_value in intersection_list:
@@ -131,265 +131,100 @@ def delete_file(filepath):
         print(f"File '{filepath}' does not exist.")
 
 
-# File definition:
-output_hd_exclude = "output_hd_exclude.raw"
-output_hd_exclude_cleaned = "output_hd_exclude_cleaned.txt"
-output_hd_exclude_binary = "output_hd_exclude_binary_herd.txt"
-output_hd_exclude_top_SNPs = "output_hd_exclude_top_SNPs.txt"
-output_hd_exclude_top_SNPs_binary = "output_hd_exclude_top_SNPs_binary.txt"
+# Expands a list to include values within the offset range for every element
+def expand_list_with_range(lst, offset):
+    expanded_list = []
+    for num in lst:
+        # Add all numbers from num-offset to num+offset (inclusive)
+        expanded_list.extend(range(num - offset, num + offset + 1))
+    return expanded_list
 
-mast_lact1 = "mast_lact1.phen"
-mast_lact1_temp = "mast_lact1_temp.txt"
-mast_lact1_sorted = "mast_lact1_sorted_herd.txt"
 
-herdxyear_lact1 = "herdxyear_lact1.covar"
-herdxyear_lact1_temp = "herdxyear_lact1_temp.txt"
-herdxyear_lact1_sorted = "herdxyear_lact1_sorted.txt"
+def get_common_cows():
+    output_hd_exclude = "output_hd_exclude.raw"
+    mast_lact1 = "mast_lact1.phen"
 
-important_SNPs = [
-    1416,
-    5636,
-    5823,
-    7935,
-    7940,
-    19157,
-    20876,
-    20877,
-    20884,
-    20888,
-    27407,
-    33710,
-    45415,
-    45416,
-    45420,
-    46205,
-    61402,
-    62252,
-    62283,
-    66036,
-    68806,
-    69607,
-    69633,
-    69637,
-    69642,
-    69643,
-    73359,
-    73360,
-    73363,
-    96064,
-    96067,
-    96072,
-    111267,
-    116968,
-    142154,
-    142234,
-    142982,
-    143776,
-    143889,
-    153588,
-    153596,
-    153604,
-    153612,
-    159306,
-    160302,
-    185257,
-    187212,
-    187213,
-    187214,
-    187228,
-    187230,
-    187345,
-    187346,
-    187347,
-    187348,
-    187350,
-    187352,
-    187353,
-    187360,
-    204015,
-    211687,
-    215340,
-    223904,
-    224028,
-    224112,
-    238523,
-    238526,
-    238527,
-    238531,
-    238538,
-    243444,
-    245187,
-    256286,
-    262054,
-    262056,
-    264309,
-    264310,
-    268836,
-    268852,
-    268969,
-    268972,
-    268973,
-    277906,
-    278951,
-    283784,
-    283785,
-    284634,
-    304160,
-    304467,
-    319642,
-    319743,
-    319745,
-    347018,
-    355488,
-    361602,
-    369271,
-    373403,
-    373545,
-    377119,
-    388236,
-    393374,
-    398409,
-    399836,
-    403564,
-    414215,
-    417407,
-    418433,
-    418465,
-    418724,
-    418779,
-    418780,
-    418782,
-    418787,
-    418788,
-    418789,
-    418791,
-    418793,
-    418794,
-    418795,
-    418798,
-    418806,
-    422601,
-    424457,
-    425749,
-    438631,
-    438632,
-    438955,
-    438956,
-    440083,
-    440592,
-    440593,
-    440594,
-    440632,
-    440666,
-    446295,
-    446297,
-    456731,
-    459311,
-    462910,
-    462911,
-    466373,
-    466600,
-    482328,
-    494527,
-    494542,
-    498202,
-    498206,
-    498207,
-    498208,
-    498214,
-    498215,
-    498217,
-    498219,
-    498220,
-    498225,
-    498226,
-    498227,
-    506192,
-    506351,
-    507818,
-    507949,
-    508687,
-    511715,
-    515701,
-    517482,
-    522448,
-    522450,
-    522453,
-    522456,
-    522749,
-    524952,
-    537378,
-    537379,
-    537402,
-    547450,
-    572088,
-    572096,
-    572098,
-    572405,
-    583635,
-    585704,
-    594478,
-    600551,
-    600776,
-    601502,
-    601505,
-    601509,
-    601521,
-    601524,
-    601529,
-    601530,
-    601580,
-    601608,
-    601609,
-    602436,
-    602454,
-    605129,
-    615474,
-    615482,
-    620200,
-    620201,
-    620219,
-]
+    # create intersection list of common cows
+    return read_and_intersect(output_hd_exclude, mast_lact1)
 
-# create intersection list of common cows
-list1, list2, intersection_list = read_and_intersect(output_hd_exclude, mast_lact1)
 
-# filter and sort mast_lact1
-print("filtering and sorting mast_lact1")
-filter_and_copy(mast_lact1, intersection_list, mast_lact1_temp)
-sort_file_by_first_column(mast_lact1_temp)
-remove_first_n_values(mast_lact1_temp, mast_lact1_sorted, 2)
+# Filters mast_lact_1 by common cows, sorts by cow ID, removes cow ID
+def clean_mastlact1():
+    # Mast Lact1 versions
+    mast_lact1 = "mast_lact1.phen"
+    mast_lact1_temp = "mast_lact1_temp.txt"
+    mast_lact1_sorted = "mast_lact1_sorted_herd.txt"
 
-# filter and sort herdxyear_lact1
-print("filtering and sorting herdxyear_lact1")
-filter_and_copy(herdxyear_lact1, intersection_list, herdxyear_lact1_temp)
-sort_file_by_first_column(herdxyear_lact1_temp)
-remove_first_n_values(herdxyear_lact1_temp, herdxyear_lact1_sorted, 2)
+    common_cows = get_common_cows()
 
-# filter and sort output_hd_exclude
-print("filtering and sorting output_hd_exclude")
-filter_and_copy(output_hd_exclude, intersection_list, output_hd_exclude_binary)
-sort_file_by_first_column(output_hd_exclude_binary)
+    print("filtering and sorting mast_lact1")
+    filter_and_copy(mast_lact1, common_cows, mast_lact1_temp)
+    sort_file_by_first_column(mast_lact1_temp)
+    remove_first_n_values(mast_lact1_temp, mast_lact1_sorted, 2)
 
-# remove first 6 cols from output_hd_exclude
-print("removing first 6 columns from output_hd_exclude")
-remove_first_n_values(output_hd_exclude_binary, output_hd_exclude_cleaned, 6)
+    # delete temp file
+    delete_file(mast_lact1_temp)
 
-# filter output_hd_exclude by significant SNPs
-print("filtering output_hd_exclude by significant SNPs")
-extract_specified_columns(
-    output_hd_exclude_cleaned, output_hd_exclude_top_SNPs, important_SNPs
-)
 
-# compress output_hd_exclude to binary
-print("compressing output_hd_exclude to binary")
-to_binary(output_hd_exclude_cleaned, output_hd_exclude_binary)
+# Filters herdxyear by common cows, sorts by cow ID, removes cow ID
+def clean_heardxyear():
+    # Heard x Year versions
+    herdxyear_lact1 = "herdxyear_lact1.covar"
+    herdxyear_lact1_temp = "herdxyear_lact1_temp.txt"
+    herdxyear_lact1_sorted = "herdxyear_lact1_sorted.txt"
 
-# compress output_hd_exclude_top_SNPs to binary
-print("compressing output_hd_exclude_top_SNPs to binary")
-to_binary(output_hd_exclude_top_SNPs, output_hd_exclude_top_SNPs_binary)
+    common_cows = get_common_cows()
 
-# delete temp files
-print("deleting mast_lact1_temp & herdxyear_lact1_temp")
-delete_file(mast_lact1_temp)
-delete_file(herdxyear_lact1_temp)
+    print("filtering and sorting herdxyear_lact1")
+    filter_and_copy(herdxyear_lact1, common_cows, herdxyear_lact1_temp)
+    sort_file_by_first_column(herdxyear_lact1_temp)
+    remove_first_n_values(herdxyear_lact1_temp, herdxyear_lact1_sorted, 2)
+
+    # delete temp file
+    delete_file(herdxyear_lact1_temp)
+
+
+# Converts SNPs from raw_data to binary format
+def clean_raw_data():
+    # All SNPS versions:
+    output_hd_exclude = "output_hd_exclude.raw"
+    output_hd_exclude_cleaned = "output_hd_exclude_cleaned.txt"
+    output_hd_exclude_binary = "output_hd_exclude_binary_herd.txt"
+
+    common_cows = get_common_cows()
+
+    # filter and sort output_hd_exclude
+    print("filtering and sorting output_hd_exclude")
+    filter_and_copy(output_hd_exclude, common_cows, output_hd_exclude_binary)
+    sort_file_by_first_column(output_hd_exclude_binary)
+
+    # remove first 6 cols from output_hd_exclude
+    print("removing first 6 columns from output_hd_exclude")
+    remove_first_n_values(output_hd_exclude_binary, output_hd_exclude_cleaned, 6)
+
+    # compress output_hd_exclude to binary
+    print("compressing output_hd_exclude to binary")
+    to_binary(output_hd_exclude_cleaned, output_hd_exclude_binary)
+
+
+# Gets binary file of top 200+ SNPs (w +/- 50 on either side)
+def get_top_200_SNPs_expanded():
+    # Top SNPs
+    top_200_SNPs_expanded = "top_200_SNPs_expanded.txt"
+    top_200_SNPs_expanded_binary = "top_200_SNPs_expanded_binary.txt"
+    top_200_SNPs_indices = "top_200_SNPs_indices.txt"
+    output_hd_exclude_cleaned = "output_hd_exclude_cleaned.raw"
+
+    # Load important SNP indices
+    important_SNPs_indices = load_1d_array_from_file(top_200_SNPs_indices)
+    important_SNPs_expanded = expand_list_with_range(important_SNPs_indices, offset=50)
+
+    # filter output_hd_exclude by significant SNPs
+    print("filtering output_hd_exclude by significant SNPs")
+    extract_specified_columns(
+        output_hd_exclude_cleaned, top_200_SNPs_expanded, important_SNPs_expanded
+    )
+
+    # compress top_snps to binary
+    print("compressing significant SNPs to binary")
+    to_binary(top_200_SNPs_expanded, top_200_SNPs_expanded_binary)
