e1fee13c12fa6e3e28db2b47504a2da9b349feea
Gavin Lynch <gavinlynch04@gmail.com>
2025-01-02 16:18:47 -0800

    deleted file

--- runTransformer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in {1..100}  # Adjust the number of iterations as needed
-do
-    echo "Starting run $i"
-    python3 ./Models/Transformer.py  # Replace with the actual script name
-    sleep 10  # Optional: Pause for 10 seconds between runs
-done
