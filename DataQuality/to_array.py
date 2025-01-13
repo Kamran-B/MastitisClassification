eed7f4550636e419435a4916b2c80994f93f59bd
BeckSchemenauer <113921526+BeckSchemenauer@users.noreply.github.com>
2025-01-13 11:57:11 -0800

    bigger chunk size

--- DataQuality/to_array.py
+++ DataQuality/to_array.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 from tqdm import tqdm
 
-def bit_reader(input_file, chunk_size=65536):
+def bit_reader(input_file, chunk_size=131072):
     """
     Optimized bit reader for faster processing of binary files.
     """
