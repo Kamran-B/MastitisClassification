0707940535f792a0e524b545e661de811d3438f8
Gavin Lynch <gavinlynch04@gmail.com>
2025-04-01 14:45:34 -0700

    working on attention/transformer

---
+++ Models/Transformer/Attention.py
@@ -0,0 +1,102 @@
+import math
+import torch.nn.functional as F
+import numpy as np
+import torch
+from torch import nn
+from torchvision.models.video.mvit import PositionalEncoding
+
+
+def xavier_2D_init(in_dim, out_dim):
+    variance = 1. / (in_dim + out_dim)
+    return np.random.uniform(-variance, variance, (in_dim, out_dim))
+
+
+def scaled_dot_product_attention(Q, K, V):
+    head_dim = K.size(-1)
+    scaling_factor = math.sqrt(head_dim)
+
+    attention_scores = torch.matmul(Q, K.transpose(-2, -1))
+
+    scaled_attention_scores = attention_scores / scaling_factor
+
+    # Apply softmax over the key sequence length dimension (last dim)
+    attention_weights = F.softmax(scaled_attention_scores, dim=-1)
+
+    # attention_weights = F.dropout(attention_weights, p=dropout_p)
+
+    output = torch.matmul(attention_weights, V)
+
+    return output
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, embed_dim, num_heads, dropout=0.1):
+        self.embedding_dim = embed_dim
+        self.num_heads = num_heads
+        assert self.embedding_dim % self.num_heads == 0
+        self.head_dim = self.embedding_dim / self.num_heads
+        self.Wq_all = xavier_2D_init(self.embedding_dim, self.embedding_dim)
+        self.Wk_all = xavier_2D_init(self.embedding_dim, self.embedding_dim)
+        self.Wv_all = xavier_2D_init(self.embedding_dim, self.embedding_dim)
+        self.Wo = xavier_2D_init(self.embedding_dim, self.embedding_dim)
+
+    def forward(self, input):
+        # Assume input is (batch_size, seq_len, embedding_dim)
+        b = input.size(0)
+        seq_len = input.size(1)
+        Q_all = input @ self.Wq_all # shape (B, seq_len, embedding_dim)
+        K_all = input @ self.Wk_all # shape (B, seq_len, embedding_dim)
+        V_all = input @ self.Wv_all # shape (B, S, embedding_dim)
+
+        # Reshape to expose the num heads vs head dimension
+        Q_all = Q_all.reshape(b, seq_len, self.num_heads, self.head_dim)
+        K_all = K_all.reshape(b, seq_len, self.num_heads, self.head_dim)
+        V_all = V_all.reshape(b, seq_len, self.num_heads, self.head_dim)
+
+        Q_split = Q_all.transpose(0, 2, 1, 3) # now (batch, num_heads, seq_len, head_dim)
+        K_split = K_all.transpose(0, 2, 1, 3)
+        V_split = V_all.transpose(0, 2, 1, 3)
+
+        output = scaled_dot_product_attention(Q_split, K_split, V_split)
+
+        contiguous_tensor = output.transpose(0, 2, 1, 3).copy()
+        concatenated_tensor = contiguous_tensor.reshape(b, seq_len, -1)
+        final_output = concatenated_tensor @ self.Wo
+        return final_output
+
+class TransformerEncoder(nn.Module):
+    def __init__(self):
+        self.embed_dim = 16
+        self.num_heads = 4
+        self.ffn_dim = 4 * self.embed_dim
+        self.dropout = nn.Dropout(0.2)
+        self.multi_head_attention = MultiHeadAttention(self.embed_dim, self.num_heads)
+        self.layer_norm = nn.LayerNorm(self.embed_dim)
+        self.layer_norm2 = nn.LayerNorm(self.embed_dim)
+        self.linear = nn.Linear(self.embed_dim, self.ffn_dim)
+        self.linear2 = nn.Linear(self.ffn_dim, self.embed_dim)
+        self.activation = nn.GELU()
+        self.dropout = nn.Dropout(0.2)
+
+    def forward(self, input):
+        '''input must be (batch_size, seq_len, embed_dim)'''
+        output = self.multi_head_attention(input)
+        output_dropout = self.dropout(output)
+        output_layer = self.layer_norm(output_dropout + input)
+        output = self.linear(output_layer)
+        output = self.activation(output)
+        output = self.linear2(output)
+        output_dropout2 = self.dropout(output)
+        output_layer2 = self.layer_norm2(output_layer + output_dropout2)
+        return output_layer2
+
+class Transformer(nn.Module):
+    def __init__(self):
+        self.num_encoder_layers = 6
+        self.num_decoder_layers = 6
+        self.vocab_size = 3
+        self.embed_dim = 16
+        self.embed = nn.Embedding(self.vocab_size, self.embed_dim)
+        self.encoders = nn.ModuleList([TransformerEncoder() for i in range(self.num_encoder_layers)])
+
+class TransformerDecoder(nn.Module):
---
+++ Models/Transformer/Main.py
@@ -0,0 +1,34 @@
+import sys
+
+from sklearn.model_selection import train_test_split
+
+from DataQuality.funtional_consequences import *
+from DataQuality.to_array import bit_reader
+from DataQuality.model_saving import *
+
+def prepare_data(seed_value, top_snps):
+    breed_herd_year = 'Data/BreedHerdYear/breed_herdxyear_lact1_sorted.txt'
+    phenotypes = 'Data/Phenotypes/phenotypes_sorted.txt'
+
+    herd = load_2d_array_from_file(breed_herd_year)
+    X = bit_reader(top_snps)
+    y = load_1d_array_from_file(phenotypes)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=seed_value, stratify=y)
+
+    return X_train, X_test, y_train, y_test
+
+
+def augment_data(X, y, seed_value):
+    X_aug, y_aug = X.copy(), y.copy()
+    duplicate_and_insert(X, X_aug, y, y_aug, 1, 16, seed=seed_value)
+    return X_aug, y_aug
+
+
+if __name__ == '__main__':
+    top_snps = "Data/TopSNPs/rf/top500_SNPs_rf_binary.txt"
+    seed = 50
+    X_train, X_test, y_train, y_test = prepare_data(seed, top_snps)
+    X_train_aug, y_train_aug = augment_data(X_train, y_train, seed)
+    X_test_aug, y_test_aug = augment_data(X_test, y_test, seed)
+    
+    '''EMBEDDING'''
